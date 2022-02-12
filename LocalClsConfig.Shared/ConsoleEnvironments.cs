c329272664a3223e2388b2344fd29eeed5c419b6
Luis D'Andrea on PC <dandrealuisdeveloper@gmail.com>
2022-02-12 18:16:48 -0400

    Version 1.0

---
+++ LocalClsConfig.Shared/ConsoleEnvironments.cs
@@ -0,0 +1,10 @@
+﻿// 2022/02/12
+// Proyecto LocalClsConfig.Shared
+
+/// <summary> </summary>
+public enum ConsoleEnvironmets {
+    /// <summary> </summary>
+    MAC,
+    /// <summary> </summary>
+    WIN
+}
---
+++ LocalClsConfig.Test/Program.cs
@@ -0,0 +1,19 @@
+﻿// 2022/02/12
+// LocalClsConfig.Test
+
+using System;
+using static LocalClsConfig;
+
+class Program {
+    static void Main(string[] args) {
+
+        SetEnvironment();
+
+        Console.WriteLine("Hello World!");
+
+        ResizeConsoleToFit();
+
+        if (ConsoleEnvironmet == ConsoleEnvironmets.WIN)
+            Console.ReadKey(true);
+    }
+}
